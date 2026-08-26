a0127f90adc0ba636e09039a9f078ad9ef7ff647
Balla.Thanuja <Balla.Thanujaballahyd@gmailc.com>
2026-08-26 10:27:44 +0530

    final check

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -3,7 +3,7 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/Features", glue = { "StepDefinitions", "Base" },tags="@TC013A", plugin = {
+@CucumberOptions(features = "src/test/java/Features", glue = { "StepDefinitions", "Base" }, plugin = {
 		"pretty", "html:target/cucumber-report.html" }, monochrome = true)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
